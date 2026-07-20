5e41b0fa47ead401297c12e73bd8ba31c1dafa9a
hhabu <nguyenphanhuybach@gmail.com>
2026-07-20 15:39:33 +0700

    style(storefront): refine related products section

--- apps/storefront/src/modules/skeletons/templates/skeleton-related-products/index.tsx
+++ apps/storefront/src/modules/skeletons/templates/skeleton-related-products/index.tsx
@@ -4,13 +4,7 @@ import SkeletonProductPreview from "@modules/skeletons/components/skeleton-produ
 const SkeletonRelatedProducts = () => {
   return (
     <div className="product-page-constraint">
-      <div className="flex flex-col gap-8 items-center text-center mb-8">
-        <div className="w-20 h-6 animate-pulse bg-gray-100"></div>
-        <div className="flex flex-col gap-4 items-center text-center mb-16">
-          <div className="w-96 h-10 animate-pulse bg-gray-100"></div>
-          <div className="w-48 h-10 animate-pulse bg-gray-100"></div>
-        </div>
-      </div>
+      <div className="mb-6 h-9 w-48 animate-pulse bg-gray-100"></div>
       <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8 flex-1">
         {repeat(3).map((index) => (
           <li key={index}>
